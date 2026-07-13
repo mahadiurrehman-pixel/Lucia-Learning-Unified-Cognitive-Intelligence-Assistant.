1298a42dea080179d3977466dec02f3cc11c56c1
Mahadi <mahadiurrehman@gmail.com>
2026-07-14 04:19:36 +0500

    lucia memory v0.1

---
+++ memory.py
@@ -0,0 +1,186 @@
+import sqlite3
+import json
+from datetime import datetime
+class LuciaMemory:
+    def __init__(self):
+        self.connection=sqlite3.connect('lucia.db')
+        self.cursor=self.connection.cursor()
+        self.create_table()
+    def create_table(self):
+        # in this version lucia has a single database with 3 tables
+        # 1 gernal memory
+        self.cursor.execute("""
+        CREATE TABLE IF NOT EXISTS memories(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            key TEXT NOT NULL,
+            value TEXT NOT NULL,
+            category TEXT DEFAULT 'general',
+            confidence REAL DEFAULT 1.0,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            access_count INTEGER DEFAULT 0)
+        """)
+        #2 conversation memory
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS conversations(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_message TEXT NOT NULL,
+            lucia_response TEXT NOT NULL,
+            emotion_detected TEXT DEFAULT 'neutral',
+            intent_detected TEXT DEFAULT 'general',
+            timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP)
+        """)
+        #3 Learned Knowledge 
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS learned_knowledge(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            topic TEXT NOT NULL,
+            contents TEXT NOT NULL,
+            source TEXT DEFAULT 'user',
+            verified INTEGER DEFAULT 0,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)
+        
+        """)
+        self.connection.commit()
+    def save_memory(self,key,value,category='general'):
+        self.cursor.execute("SELECT id FROM memories WHERE key = ? AND category = ?",(key,category))
+        existing =  self.cursor.fetchone()
+        if existing:
+            self.cursor.execute("""
+                UPDATE memories
+                SET value = ?, updated_at = ?
+                WHERE key = ? AND category = ?
+            """,(value,datetime.now(),key,category))
+            print(f'[MEMORY] is updated: {key} = {value}')
+        else:
+            #new created if not exist 
+            self.cursor.execute("""
+                INSERT INTO memories(key, value, category)
+                VALUES (?,?,?)
+            """,(key,value,category))
+            print(f'[MEMORY] created: {key} = {value} [{category}] ')
+        self.connection.commit()
+        return True
+    def get_memory(self,key,category=None):
+        #recall memory
+        if category:
+            #search through a specific category told by user
+            self.cursor.execute("SELECT value FROM memories WHERE key=? AND category=?",
+            (key,category))
+        else:
+            #Randomly search
+            self.cursor.execute("SELECT value FROM memories WHERE key=?",(key,))
+        result=self.cursor.fetchone()
+        if result:
+            #it will tell how much values it checked
+            self.cursor.execute("UPDATE memories SET access_count=access_count +1 WHERE key=?",(key,))
+            self.connection.commit()
+            print (f"[MEMORY] Found: {key} ={result[0]}")
+            return result[0]
+        print(f"[MEMORY] NOT FOUND: {key}")
+        return None
+    def search_memory(self,search_term):
+        self.cursor.execute("""
+            SELECT key, value, category, created_at
+            FROM memories
+            WHERE key LIKE ? OR value LIKE ? OR category LIKE ? 
+            ORDER BY updated_at DESC
+            LIMIT 10
+        """,(f"%{search_term}%",f"%{search_term}%",f"%{search_term}%"))
+        results = self.cursor.fetchall()
+        if  results:
+            print(f"[MEMORY] found {len(results)} results for '{search_term}'")
+            return[
+                {
+                    "key":r[0],
+                    "value":r[1],
+                    "category":r[2],
+                    "created_at":r[3]
+                }
+                for r in results
+            ]
+        return[]
+    def get_all_memories(self,category=None):
+        #it show all memories
+        if category:
+            self.cursor.execute(
+                "SELECT key, value, category, created_at FROM memories WHERE category =?",
+                (category,)
+            )
+        else:
+            self.cursor.execute("SELECT key, value, category, created_at FROM memories")
+        results=self.cursor.fetchall()
+        return[
+            {
+                "key":r[0],
+                "value":r[1],
+                "category":r[2],
+                "created_at":r[3]
+            }
+            for r in results
+        ]
+    def save_conversation(self, user_message, lucia_response,emotion_detected, intent_detected):
+        self.cursor.execute("""
+            INSERT INTO conversations
+            (user_message, lucia_response, emotion_detected, intent_detected)
+            VALUES (?,?,?,?)
+        """,(user_message,lucia_response,emotion_detected,intent_detected))
+        self.connection.commit()
+    def save_learned_info(self, topic, contents, verified=False):
+        self.cursor.execute("""
+            INSERT INTO learned_knowledge (topic,contents,verified)
+            VALUES (?,?,?)
+        """,(topic,contents,1 if verified else 0))
+        self.connection.commit()
+        print(f"[LEARNING] Learned: {topic}")
+    def get_learned_info(self, topic):
+        self.cursor.execute("""
+            SELECT contents, verified FROM learned_knowledge
+            WHERE topic LIKE ?
+            ORDER BY created_at DESC
+        """,(f"%{topic}%",))
+        results=self.cursor.fetchall()
+        return[
+            {
+                "content":r[0],
+                "verified":bool(r[1])
+            }for r in results
+        ]
+    def get_recent_conversations(self,limit=5):
+        self.cursor.execute("""
+            SELECT user_message, lucia_response, emotion_detected,intent_detected,timestamp FROM conversations
+            ORDER BY timestamp DESC LIMIT ?
+        """,(limit,))
+        results=self.cursor.fetchall()
+        return[
+            {
+                "user":r[0],
+                "lucia":r[1],
+                "emotion":r[2],
+                "intent":r[3],
+                "time":r[4]
+            }for r in results
+        ]
+    def delete_memory(self,key):
+        self.cursor.execute("DELETE FROM memories WHERE key=?",(key,))
+        self.connection.commit()
+        print(f"[MEMORY]  Deleted: {key}")
+    def close(self):
+        self.connection.close()
+        print("[MEMORY]  Memory system closed.")
+# if __name__ == "__main__":
+#     mem = LuciaMemory()
+
+#     # Test save
+#     mem.save_memory("name", "Mahadi", "personal_info")
+
+#     # Test get
+#     print(mem.get_memory("name"))
+
+#     # Test search
+#     print(mem.search_memory("Mahadi"))
+
+#     # Test all
+#     print(mem.get_all_memories())
+
+#     mem.close()
